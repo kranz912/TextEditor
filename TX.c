9a07cbdea72c6f3d7fa7b76d6e5b15b99a741f3e
kranz fontanilla <me@kranznikolai.com>
2019-11-04 16:15:58 -0500

    added main

--- TX.c
+++ TX.c
@@ -1,4 +1,13 @@
 #include <unistd.h>
+#include <termios.h>
+void enableRawMode(){
+    struct  termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+    tcgetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+
 
 int main(){
     char c;
